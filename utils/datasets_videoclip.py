6cd4b6411318716bab57179000891b3a2d34d1b5
cwLin1 <61427980+cwLin1@users.noreply.github.com>
2022-12-19 09:54:51 +0800

    video feature

--- utils/datasets_videoclip.py
+++ utils/datasets_videoclip.py
@@ -30,9 +30,9 @@ class Ego4D(Dataset):
         super(Ego4D).__init__()
         self.video_dir = os.path.join(data_dir, 'videos')
         self.audio_dir = os.path.join(data_dir, 'audios')
-        self.feature_dir = os.path.join(data_dir, 'i3d')
         self.seg_dir = os.path.join(data_dir, 'train', 'seg')
-
+        self.bbox_dir = os.path.join(data_dir, 'train', 'bbox')
+        
         self.video_ids = sorted([x.split('_')[0] for x in os.listdir(self.seg_dir)])
         if subset == 'train':
             self.video_ids = self.video_ids[:split]
@@ -43,31 +43,69 @@ class Ego4D(Dataset):
         self.bbox = {}
         for video_id in self.video_ids:
             seg_path = os.path.join(self.seg_dir, video_id + '_seg.csv')
+            bbox_path = os.path.join(self.bbox_dir, video_id + '_bbox.csv')
             with open(seg_path, newline='') as csvfile:
                 rows = csv.DictReader(csvfile)
                 for row in rows:
                     row['video_id'] = video_id
                     self.seg.append(row)
 
+            with open(bbox_path, newline='') as csvfile:
+                rows = csv.DictReader(csvfile)
+                self.bbox[video_id] = []
+                for row in rows:
+                    self.bbox[video_id].append(row)
+
         self.transform = tfm
+    
+    def get_bbox(self, bbox_dict, person_id, start_frame, end_frame):
+        bbox = []
+        for row in bbox_dict:
+            if row['person_id'] == person_id and int(row['frame_id']) >= start_frame and int(row['frame_id']) < end_frame:
+                bbox.append([int(np.floor(float(row['x1']))), int(np.floor(float(row['x2']))), 
+                            int(np.ceil(float(row['y1']))), int(np.ceil(float(row['y2'])))])
+        return bbox
 
     def __len__(self):
         return len(self.seg)
   
     def __getitem__(self, idx):
         seg_id = self.seg[idx]
+        print(seg_id)
         video_id = seg_id['video_id']
         person_id = seg_id['person_id']
         start_frame = int(seg_id['start_frame'])
         end_frame = int(seg_id['end_frame'])
         label = float(seg_id['ttm'])
 
-        feature_id = video_id + '_' + person_id + '_' + start_frame + '_' + end_frame
-        feature_path = os.path.join(self.feature_dir, feature_id + '.npy')
+        video_path = os.path.join(self.video_dir, video_id + '.mp4')
         audio_path = os.path.join(self.audio_dir, video_id + '.wav')
 
-        video_feature = np.load(feature_path)
+        cap = cv2.VideoCapture(video_path)
+        cap.set(1, start_frame)
+        frame_num = start_frame
 
+        segment = torch.zeros(0, 3, 224, 224)
+        ret, frame = cap.read() # Read the frame
+        bbox = self.get_bbox(self.bbox[video_id], person_id, start_frame, end_frame)
+        while ret and frame_num < end_frame:
+            x1, x2, y1, y2 = bbox[frame_num-start_frame]
+            if x1 == -1 and x2 == -1 and y1 == -1 and y2 == -1:
+                segment = torch.cat((segment, torch.zeros(1, 3, 224, 224)), 0)
+            else:
+                image = frame[y1:y2, x1:x2]
+                image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+                image = self.transform(image).unsqueeze(0)
+                segment = torch.cat((segment, image), 0)
+
+            ret, frame = cap.read()
+            frame_num += 1
+
+        sz = segment.size(0)
+        padding_size = 64 * int(np.ceil((sz)/64)) - sz
+        segment = torch.cat((segment, torch.zeros(padding_size, 3, 224, 224)), 0)
+        segment = segment.view(segment.size(0)//64, 64, 3, 224, 224)
         ori_audio, ori_sample_rate = torchaudio.load(audio_path, normalize = True)
         sample_rate = 16000
         audio_resample = torchaudio.transforms.Resample(ori_sample_rate, sample_rate)
@@ -82,13 +120,8 @@ class Ego4D(Dataset):
         crop_audio = crop_audio.view(int(np.ceil((offset-onset)/34134)), 2, 34134)
         # crop_audio = audio[:, onset:offset]
         audio_transform = torchaudio.transforms.MFCC()
-        audio_feature = audio_transform(crop_audio)
 
-        # ======
-        # video_feature: n x 1024
-        # audio_feature: n x 2 x 40 x 171
-        # ======
-        return video_feature, audio_feature, label
+        return segment, audio_transform(crop_audio), label
 
 def get_train_val_loader(path, split):
     num_train = len(os.listdir(os.path.join(path, 'train', 'seg')))
